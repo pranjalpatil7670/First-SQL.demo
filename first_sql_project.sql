c9947b62c8d4ffe8ed40ce6e6743c530cafcd912
pranjalpatil7670 <pranjalpatil7670@gmail.com>
2026-09-03 19:23:18 +0530

    Create students table and insert initial data

---
+++ first_sql_project.sql
@@ -0,0 +1,13 @@
+CREATE TABLE students (
+  id INT PRIMARY KEY,
+  name VARCHAR (50),
+  age INT NOT NULL 
+);
+
+
+INSERT INTO students VALUES (1, 'mau', 20);
+INSERT INTO students VALUES (2, 'khushi', 21);
+
+SELECT * FROM students;
+
+
